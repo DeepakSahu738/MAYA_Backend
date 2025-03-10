f98ce3f01a08ad4ed3d5f8f965ea59e4f828cd06
deepak sahu <DEEPAK738.SAHU@GMAIL.COM>
2025-03-11 05:11:34 +0530

    ai response error handling

--- src/main/java/com/MAYA/MAYA/Controller/instaGramController.java
+++ src/main/java/com/MAYA/MAYA/Controller/instaGramController.java
@@ -3,6 +3,8 @@ package com.MAYA.MAYA.Controller;
 import com.MAYA.MAYA.DTO.instagram.*;
 import com.MAYA.MAYA.Service.instaGramService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -18,39 +20,59 @@ public class instaGramController {
     @Autowired
     private instaGramService instaGramService;
     @PostMapping("/insta_one")
-    public Map<String, String> generateContentIdea(@RequestBody ContentIdeaDTO request) {
+    private ResponseEntity<Map<String, String>> generateContentIdea(@RequestBody ContentIdeaDTO request) {
+        //we are doing the LangChain stuff in the service section
+        try {
+            String generateCoreIdea = instaGramService.generateContentIdeas(request.getContentGoal(),request.getNiche(),request.getContentType()
+                    ,request.getTrendingOrEvergreen(),request.getTargetAudience());
+            Map<String, String> response = new HashMap<>();
+            response.put("contentIdea", generateCoreIdea);
+            return  new ResponseEntity<>(response, HttpStatus.OK);
 
-        // Simulated response (Later, we will integrate LangChain AI here)
-        String generateCoreIdea = instaGramService.generateContentIdeas(request.getContentGoal(),request.getNiche(),request.getContentType()
-        ,request.getTrendingOrEvergreen(),request.getTargetAudience());
+        } catch (Exception e) {
 
-        // Returning a JSON response  not using the instaService currently will integrate when we integrate Langchain AI
-        Map<String, String> response = new HashMap<>();
-        response.put("contentIdea", generateCoreIdea);
-        return response;
+            Map<String, String> response = new HashMap<>();
+            response.put("ERROR", "⚠️ Error: Unable to generate content ideas. Please try again later.");
+            return  new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/insta_two")
-    public Map<String, String> generateCaption(@RequestBody CaptionDTO request) {
+    private ResponseEntity<Map<String, String>> generateCaption(@RequestBody CaptionDTO request) {
 
-        // Simulated response (Later, we will integrate LangChain AI here)
-
-        String generateCaptionWithCTA = instaGramService.generateCaptionWithCTA(request.getContentIdea()
-                ,request.getToneStyle(),request.getCallToAction());
-
+        //we are doing the LangChain stuff in the service section
+        try {
+            String generateCaptionWithCTA = instaGramService.generateCaptionWithCTA(request.getContentIdea()
+                    , request.getToneStyle(), request.getCallToAction());
+            Map<String, String> response = new HashMap<>();
+            response.put("caption", generateCaptionWithCTA);
+            return  new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        catch (Exception e){
+            Map<String, String> response = new HashMap<>();
+            response.put("ERROR", "⚠️ Error: Unable to generate content ideas. Please try again later.");
+            return  new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         // Returning a JSON response  not using the instaService currently will integrate when we integrate Langchain AI
-        Map<String, String> response = new HashMap<>();
-        response.put("caption", generateCaptionWithCTA);
-        return response;
     }
 
     @PostMapping("/insta_three")
-    public Map<String, List<String>> generateHashtags(@RequestBody HashtagsDTO request) {
+    private ResponseEntity<Map<String, String>> generateHashtags(@RequestBody HashtagsDTO request) {
         List<String> generatedHashtags = new ArrayList<>();
 
-        // Simulating hashtag generation (Later, we will integrate LangChain AI)
-        String generatedHashtags1 =  instaGramService.suggestHashtags(request.getNiche(),request.getKeywords(),
-                request.getTrendingOrEvergreen());
+        //we are doing the LangChain stuff in the service section
+        try {
+            String generatedHashtags1 = instaGramService.suggestHashtags(request.getNiche(), request.getKeywords(),
+                    request.getTrendingOrEvergreen());
+            Map<String, String> response = new HashMap<>();
+            response.put("hashtags", generatedHashtags1);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        catch (Exception e){
+            Map<String, String> response = new HashMap<>();
+            response.put("ERROR", "⚠️ Error: Unable to generate content ideas. Please try again later.");
+            return  new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 //        for (String keyword : request.getKeywords()) {
 //            generatedHashtags.add("#" + keyword.replace(" ", "").toLowerCase());
 //        }
@@ -63,47 +85,61 @@ public class instaGramController {
 //        }
 
         // Returning a JSON response / not using the instaService currently will integrate when we integrate Langchain AI
-        Map<String, List<String>> response = new HashMap<>();
-        response.put("hashtags", generatedHashtags);
-        return response;
     }
 
 
     @PostMapping("/insta_four")
-    public Map<String, String> generateDesignAndSuggestion(@RequestBody DesignDTO request) {
+    private ResponseEntity<Map<String, String>> generateDesignAndSuggestion(@RequestBody DesignDTO request) {
 
-        // Simulated response (Later, we will integrate LangChain AI)
-
-        String generateDesignAestheticSugestion =instaGramService.suggestDesignAndAesthetic(request.getNiche(),
-                request.getToneStyle(),request.getContentType());
-
-        // Returning a JSON response not using the instaService currently will integrate when we integrate Langchain AI
-        Map<String, String> response = new HashMap<>();
-        response.put("hook", generateDesignAestheticSugestion);
-        return response;
+        //we are doing the LangChain stuff in the service section
+        try {
+            String generateDesignAestheticSugestion = instaGramService.suggestDesignAndAesthetic(request.getNiche(),
+                    request.getToneStyle(), request.getContentType());
+            Map<String, String> response = new HashMap<>();
+            response.put("hook", generateDesignAestheticSugestion);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        catch (Exception e) {
+            // Returning a JSON response not using the instaService currently will integrate when we integrate Langchain AI
+            Map<String, String> response = new HashMap<>();
+            response.put("ERROR", "⚠️ Error: Unable to generate content ideas. Please try again later.");
+            return  new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     @PostMapping("/insta_five")
-    public Map<String, String> generateStrategyForEngagement(@RequestBody EngagementDTO request) {
-        // Simulated response (Later, we will integrate LangChain AI)
-        String generateEngagementTips = instaGramService.generateEngagementStrategies(request.getContentGoal(),request.getTargetAudience());
-
+    private ResponseEntity<Map<String, String>> generateStrategyForEngagement(@RequestBody EngagementDTO request) {
+        //we are doing the LangChain stuff in the service section
+        try {
+            String generateEngagementTips = instaGramService.generateEngagementStrategies(request.getContentGoal(), request.getTargetAudience());
+            Map<String, String> response = new HashMap<>();
+            response.put("strategy", generateEngagementTips);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
         // Returning a JSON response
-        Map<String, String> response = new HashMap<>();
-        response.put("strategy", generateEngagementTips);
-        return response;
+        catch (Exception e) {
+            Map<String, String> response = new HashMap<>();
+            response.put("ERROR", "⚠️ Error: Unable to generate content ideas. Please try again later.");
+            return  new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/insta_six")
-    public Map<String, String> generateContentPostingTime(@RequestBody PostingTimeDTO request) {
-        // Simulated response (Later, we will integrate LangChain AI)
-        String generateBestPostingTime = instaGramService.suggestBestPostTime(request.getNiche(),request.getTargetAudience());
-
+    private ResponseEntity<Map<String, String>> generateContentPostingTime(@RequestBody PostingTimeDTO request) {
+        //we are doing the LangChain stuff in the service section
+        try {
+            String generateBestPostingTime = instaGramService.suggestBestPostTime(request.getNiche(), request.getTargetAudience());
+            Map<String, String> response = new HashMap<>();
+            response.put("contentAdvice", generateBestPostingTime);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
         // Returning a JSON response
-        Map<String, String> response = new HashMap<>();
-        response.put("contentAdvice", generateBestPostingTime);
-        return response;
+        catch (Exception e){
+            Map<String, String> response = new HashMap<>();
+            response.put("ERROR", "⚠️ Error: Unable to generate content ideas. Please try again later.");
+            return  new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
