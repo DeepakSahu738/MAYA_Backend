c20cfbd3d32d779a81feb77cf95d7cdb444cbf22
deepak sahu <DEEPAK738.SAHU@GMAIL.COM>
2025-05-01 17:25:10 +0530

    User Id and role added in the Access token and added get role/id function in the jwttokenprovider

--- src/main/java/com/MAYA/MAYA/Security/jwtTokenProvider.java
+++ src/main/java/com/MAYA/MAYA/Security/jwtTokenProvider.java
@@ -1,11 +1,15 @@
 package com.MAYA.MAYA.Security;
 
+import com.MAYA.MAYA.Entity.user;
+import com.MAYA.MAYA.Repository.userRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -16,6 +20,8 @@ public class jwtTokenProvider {
     private String JWT_SECRET ;
 
     private final long JWT_EXPIRATION = 86400000L; // 1 day
+    @Autowired
+    private com.MAYA.MAYA.Repository.userRepository userRepository; // This should be the repository that interacts with your database
 
     public String generateToken(Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
@@ -23,16 +29,35 @@ public class jwtTokenProvider {
         String role = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).findFirst().orElse(null);
         System.out.println("auth ye aaya "+role);
 
+        user user = userRepository.findByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        Long userID = user.getUserId();
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + JWT_EXPIRATION);
 
         return Jwts.builder()
                 .setSubject(username)
+                .claim("userID", userID)
                 .claim("role",role)
                 .setIssuedAt(new Date())
                 .setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS256, JWT_SECRET)
                 .compact();
+    }
+
+    public long getUserIDFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .get("userID", Long.class); //  Extract the userID claim
+    }
+    public String getUserRoleFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role", String.class); //  Extract the userRole claim
     }
 
     public String getUsernameFromToken(String token) {
